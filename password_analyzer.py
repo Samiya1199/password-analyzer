28546b5a65483aae30412c3347fd5245978685b0
Samiya K <khabibullaevasamiya@gmail.com>
2025-12-11 07:36:31 +0300

    İnitial Commit

---
+++ password_analyzer.py
@@ -0,0 +1,80 @@
+import math
+
+# password_analyzer.py
+
+# Minimum uzunluk kontrolü
+def has_min_length(pw, min_length=12):
+    return len(pw) >= min_length
+
+# Büyük harf kontrolü
+def has_upper(pw):
+    for c in pw:
+        if c.isupper():
+            return True
+    return False
+
+# Küçük harf kontrolü
+def has_lower(pw):
+    for c in pw:
+        if c.islower():
+            return True
+    return False
+
+# Rakam kontrolü
+def has_digit(pw):
+    for c in pw:
+        if c.isdigit():
+            return True
+    return False
+
+# Özel karakter kontrolü
+def has_special(pw):
+    special_chars = "!@#$%^&*()-_+=<>?/.,:;[]{}|~`"
+    for c in pw:
+        if c in special_chars:
+            return True
+    return False
+
+# Entropy hesaplama fonksiyonu
+
+def calculate_entropy(pw):
+    # Her karakterin frekansını say
+    freq = {}
+    for c in pw:
+        freq[c] = freq.get(c, 0) + 1
+
+    # Shannon entropy formülü
+    entropy = 0
+    length = len(pw)
+    for f in freq.values():
+        p = f / length
+        entropy -= p * math.log2(p)
+    return entropy
+
+# Ana analiz fonksiyonu
+def analyze_password(pw):
+    results = {
+        "Min Length": has_min_length(pw),
+        "Uppercase": has_upper(pw),
+        "Lowercase": has_lower(pw),
+        "Digit": has_digit(pw),
+        "Special Character": has_special(pw),
+        "Entropy >= 3.5": calculate_entropy(pw) >= 3.5,
+    }
+
+
+    # Basit Strength değerlendirmesi
+    passed = sum(value for value in results.values())
+    if passed == len(results):
+        strength = "Güçlü"
+    elif passed >= len(results) / 2:
+        strength = "Orta"
+    else:
+        strength = "Zayıf"
+
+    print(f"Password Strength: {strength}")
+
+# Kullanıcıdan parola al
+if __name__ == "__main__":
+    password = input("Enter your password: ")
+    analyze_password(password)
